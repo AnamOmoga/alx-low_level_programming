fdb9c41eabb5076e55e2c23bde1331899c0da623
Your Name <anamomoga@gmail.com>
2023-04-10 13:13:34 -0700

    3-strspn

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+void _puts(char *str)
+#include <stdio.h>
+
+	/**
+	 * main - Entry poiint
+	 *
+	 * Return: Always 0 (Success)
+	 */
+
+	int main(void)
+{
+	puts("\"Programming is like building a mulitlingual puzzle");
+	return (0);
+}
