2b93ae20acb81ee4a403cc6327d726a427e5dd77
Your Name <anamomoga@gmail.com>
2023-04-17 12:16:52 -0700

    New gcc files

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -79,14 +79,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 
 	dog->owner = malloc(sizeof(char) * (len2 + 1));
-	if (d0g->owner == NULL)
+	if (dog->owner == NULL)
 	{
 		free(dog);
 		free(dog->name);
 		return (NULL);
 	}
 	_strcpy(dog->name, name);
-	_strcpyy(dog->owner, owner);
+	_strcpy(dog->owner, owner);
 	dog->age = age;
 
 	return (dog);
