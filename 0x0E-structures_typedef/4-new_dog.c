ead163f74972a627d04dd9fbfe735db1d69af1d1
Your Name <anamomoga@gmail.com>
2023-04-17 12:01:59 -0700

    0x0E-structures_typedef main files and gcc

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -78,7 +78,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	dog->owner = malloc(sizeof(char) * (len2 +1));
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
 	if (d0g->owner == NULL)
 	{
 		free(dog);
